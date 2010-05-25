ef9fa32ef17bb79f8247987e4747e5d244292c46
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-05-25 16:52:04 -0600

    Adding the specs for the new comparisons

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/collections/MovieLibrary.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/collections/MovieLibrary.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using nothinbutdotnetprep.utility.extensions;
 
 namespace nothinbutdotnetprep.collections
@@ -23,52 +22,6 @@ namespace nothinbutdotnetprep.collections
             if (movies.Contains(movie)) return;
 
             movies.Add(movie);
-        }
-
-        public IEnumerable<Movie> sort_all_movies_by_title_ascending
-        {
-            get
-            {
-                var comparer = new MovieComparer();
-                comparer.ComparisonType = ComparisonType.Title;
-                ((List<Movie>) movies).Sort(comparer);
-                return movies;
-            }
-        }
-
-        public IEnumerable<Movie> sort_all_movies_by_title_descending
-        {
-            get
-            {
-                var comparer = new MovieComparer();
-                comparer.ComparisonType = ComparisonType.Title;
-                ((List<Movie>) movies).Sort(comparer);
-                return movies.Reverse();
-            }
-        }
-
-        public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
-        {
-            var comparer = new MovieComparer();
-            comparer.ComparisonType = ComparisonType.RatingDatePublished;
-            ((List<Movie>) movies).Sort(comparer);
-            return movies;
-        }
-
-        public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
-        {
-            var comparer = new MovieComparer();
-            comparer.ComparisonType = ComparisonType.DatePublished;
-            ((List<Movie>) movies).Sort(comparer);
-            return movies.Reverse();
-        }
-
-        public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
-        {
-            var comparer = new MovieComparer();
-            comparer.ComparisonType = ComparisonType.DatePublished;
-            ((List<Movie>) movies).Sort(comparer);
-            return movies;
         }
     }
 }
