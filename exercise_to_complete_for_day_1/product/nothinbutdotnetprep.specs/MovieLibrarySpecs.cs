4348222698b8b77bb1c2b539d893b0491d31dae9
Magesh <tkmagesh77@gmail.com>
2010-05-25 23:34:49 +0530

    following the coding conventions

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep.specs/MovieLibrarySpecs.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep.specs/MovieLibrarySpecs.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using Machine.Specifications;
 using Machine.Specifications.DevelopWithPassion.Extensions;
 using Machine.Specifications.DevelopWithPassion.Rhino;
--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/Where.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/Where.cs
@@ -1,20 +1,17 @@
 using System.Collections.Generic;
 namespace nothinbutdotnetprep.utility.searching
 {
-    public delegate PropertyType PropertyAccessor<ItemToFilter,PropertyType>(ItemToFilter item);
+    public delegate PropertyType PropertyAccessor<ItemToFilter, PropertyType>(ItemToFilter item);
 
     public class Where<ItemToFilter>
     {
-
-       public static CriteriaFactory<ItemToFilter,PropertyType> has_a<PropertyType>(
-            PropertyAccessor<ItemToFilter,PropertyType> accessor)
+        public static CriteriaFactory<ItemToFilter, PropertyType> has_a<PropertyType>(
+             PropertyAccessor<ItemToFilter, PropertyType> accessor)
         {
-          
+
             return new CriteriaFactory<ItemToFilter, PropertyType>(accessor);
         }
-
-        
-      }
+    }
 
     public class CriteriaFactory<ItemToFilter, PropertyType>
     {
@@ -25,43 +22,35 @@ namespace nothinbutdotnetprep.utility.searching
             this.accessor = accessor;
         }
 
-        public PredicateCriteria<ItemToFilter> equal_to(PropertyType propertyValue)
+        public Criteria<ItemToFilter> equal_to(PropertyType property_value)
         {
-            return new PredicateCriteria<ItemToFilter>(x => accessor(x).Equals(propertyValue));
+            return new PredicateCriteria<ItemToFilter>(x => accessor(x).Equals(property_value));
         }
 
-        public PredicateCriteria<ItemToFilter> not_equal_to(PropertyType propertyValue)
+        public Criteria<ItemToFilter> not_equal_to(PropertyType property_value)
         {
-            return new PredicateCriteria<ItemToFilter>(x => !accessor(x).Equals(propertyValue));
+            //return new PredicateCriteria<ItemToFilter>(x => !accessor(x).Equals(propertyValue));
+            return new NotCriteria<ItemToFilter>(equal_to(property_value));
         }
 
-        public PredicateCriteria<ItemToFilter> equal_to_any(params PropertyType[] propertyValues)
+        public Criteria<ItemToFilter> equal_to_any(params PropertyType[] property_values)
         {
-            return new PredicateCriteria<ItemToFilter>(x =>
-            {
-                foreach (var propertyValue in propertyValues)
-                {
-                    if (accessor(x).Equals(propertyValue))
-                        return true;
-                }
-                return false;
-            });
+            return new PredicateCriteria<ItemToFilter>(x => new List<PropertyType>(property_values).Contains(accessor(x)));
+
+            /* return new PredicateCriteria<ItemToFilter>(x =>
+             {
+                 foreach (var propertyValue in propertyValues)
+                 {
+                     if (accessor(x).Equals(propertyValue))
+                         return true;
+                 }
+                 return false;
+             });*/
         }
 
-        public PredicateCriteria<ItemToFilter> equal_to_all(params PropertyType[] propertyValues)
-        {
-            return new PredicateCriteria<ItemToFilter>(x =>
-            {
-                foreach (var propertyValue in propertyValues)
-                {
-                    if (!accessor(x).Equals(propertyValue))
-                        return false;
-                }
-                return true;
-            });
-        }
-       
-     
-       
+
+
+
+
     }
 }
