b697d4f70ed7b0ece8baac418a955daf54f0d37e
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-05-25 20:30:49 -0600

    Removed the ComparerExtensions

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/ComparerExtensions.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/ComparerExtensions.cs
@@ -1,19 +1,12 @@
-using System;
 using System.Collections.Generic;
 
 namespace nothinbutdotnetprep.utility.sorting
 {
     public static class ComparerExtensions
     {
-        public static IComparer<ItemToSort> then_by<ItemToSort,PropertyType>(this IComparer<ItemToSort> comparer,
-            Func<ItemToSort,PropertyType> accessor) where PropertyType : IComparable<PropertyType>
-        {
-            return new CombinedComparer<ItemToSort>(comparer, Sort<ItemToSort>.by(accessor));
-        }
-
         public static IComparer<T> reverse<T>(this IComparer<T> comparer)
         {
-            return new ReverseComparer<T>(comparer); 
+            return new ReverseComparer<T>(comparer);
         }
     }
 }
