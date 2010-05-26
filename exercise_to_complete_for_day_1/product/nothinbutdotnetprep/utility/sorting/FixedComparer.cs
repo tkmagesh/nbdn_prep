e0947cad3d961fb882ad5be8865011c09bcbb250
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-05-25 20:05:51 -0600

    Updated the FixedComparer

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/FixedComparer.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/FixedComparer.cs
@@ -5,9 +5,18 @@ namespace nothinbutdotnetprep.utility.sorting
 {
     public class FixedComparer<ItemToSort,PropertyType> :IComparer<ItemToSort>
     {
+        Func<ItemToSort, PropertyType> accessor;
+        IList<PropertyType> values;
+
+        public FixedComparer(Func<ItemToSort, PropertyType> accessor, IEnumerable<PropertyType> values)
+        {
+            this.accessor = accessor;
+            this.values = new List<PropertyType>(values);
+        }
+
         public int Compare(ItemToSort x, ItemToSort y)
         {
-            throw new NotImplementedException();
+            values.IndexOf(accessor(x)).CompareTo(values.IndexOf(accessor(y)));
         }
     }
 }
