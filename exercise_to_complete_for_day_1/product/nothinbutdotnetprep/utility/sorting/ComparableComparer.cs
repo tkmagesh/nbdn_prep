e855a337b37bb4be376ba8bc3e00c2b34b993519
Magesh <tkmagesh77@gmail.com>
2010-05-26 07:59:35 +0530

    sorting exercise first pass

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/ComparableComparer.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/ComparableComparer.cs
@@ -17,4 +17,27 @@ namespace nothinbutdotnetprep.utility.sorting
             return accessor(x).CompareTo(accessor(y));
         }
     }
+
+    public class ComparableComparerWithCustomSortOrder<ItemToCompare, PropertyType> : IComparer<ItemToCompare>
+    {
+        Func<ItemToCompare, PropertyType> accessor;
+        IEnumerable<PropertyType> values;
+        Dictionary<PropertyType, int> sortOrder;
+
+        public ComparableComparerWithCustomSortOrder(Func<ItemToCompare, PropertyType> accessor, PropertyType[] values)
+        {
+            this.accessor = accessor;
+            this.values = values;
+
+            sortOrder = new Dictionary<PropertyType, int>();
+            for(int i=0; i < values.Length; i++)
+                sortOrder.Add(values[i], i);
+
+        }
+
+        public int Compare(ItemToCompare x, ItemToCompare y)
+        {
+            return sortOrder[accessor(x)].CompareTo(sortOrder[accessor(y)]);
+        }
+    }
 }
--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/Sort.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/Sort.cs
@@ -8,7 +8,8 @@ namespace nothinbutdotnetprep.utility.sorting
         public static IComparer<ItemToSort> by<PropertyType>(Func<ItemToSort, PropertyType> accessor,
                                                              params PropertyType[] values)
         {
-            throw new NotImplementedException();
+
+            return new ComparableComparerWithCustomSortOrder<ItemToSort, PropertyType>(accessor, values); 
         }
 
         public static IComparer<ItemToSort> by<PropertyType>(Func<ItemToSort, PropertyType> accessor)
@@ -21,6 +22,19 @@ namespace nothinbutdotnetprep.utility.sorting
             where PropertyType : IComparable<PropertyType>
         {
             return by(accessor).reverse();
+        }
+
+      
+    }
+
+    public static class MyExtensions
+    {
+        public static IComparer<ItemToSort> then_by<ItemToSort, PropertyType>(this IComparer<ItemToSort> firstComparer, Func<ItemToSort, PropertyType> accessor) where PropertyType : IComparable<PropertyType>
+        {
+           /* return new CombinedComparer<ItemToSort>(firstComparer,
+                                                    new ComparableComparer<ItemToSort, PropertyType>(accessor));*/
+
+            return new CombinedComparer<ItemToSort>(firstComparer, Sort<ItemToSort>.by(accessor));
         }
     }
 }
